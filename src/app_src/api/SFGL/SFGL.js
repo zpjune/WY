cfd2d130429df429fe97760a4e93ff0073a73fe6
tengfei8771 <532050072@qq.com>
2020-03-27 17:18:22 +0800

    11.。。

---
+++ src/app_src/api/SFGL/SFGL.js
@@ -0,0 +1,71 @@
+import request from '@/frame_src/utils/request'
+
+export function GetFeeResult(query) {
+  return request({
+    url: '/FeeResult/GetFeeResult',
+    method: 'get',
+    params: query
+  })
+}
+export function GetShopInfo(query) {
+  return request({
+    url: '/FeeResult/GetShopInfo',
+    method: 'get',
+    params: query
+  })
+}
+
+export function CreateNotification(query) {
+  return request({
+    url: '/FeeResult/CreateNotification',
+    method: 'get',
+    params: query
+  })
+}
+
+export function ConfirmNotificationList(data) {
+  return request({
+    url: '/FeeResult/ConfirmNotificationList',
+    method: 'post',
+    data
+  })
+}
+
+export function PushNotification(data) {
+  return request({
+    url: '/FeeResult/PushNotification',
+    method: 'post',
+    data
+  })
+}
+
+export function GetHistoryFeeResult(query) {
+  return request({
+    url: '/FeeResult/GetHistoryFeeResult',
+    method: 'get',
+    params: query
+  })
+}
+export function GetBadFeeResult(query) {
+  return request({
+    url: '/FeeResult/GetBadFeeResult',
+    method: 'get',
+    params: query
+  })
+}
+
+export function ConfirmFee(query) {
+  return request({
+    url: '/FeeResult/ConfirmFee',
+    method: 'get',
+    params: query
+  })
+}
+
+export function ConfirmReciveMoney(data) {
+  return request({
+    url: '/FeeResult/ConfirmReciveMoney',
+    method: 'post',
+    data
+  })
+}
